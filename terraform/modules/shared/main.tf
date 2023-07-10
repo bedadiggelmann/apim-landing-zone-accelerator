3248cf11b61cf53bc51233b8cbddaa62a2f7c15e
Joachim Huber <joachim.huber@ipt.ch>
2023-07-10 16:28:34 +0200

    Comment access policy

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -32,7 +32,7 @@ locals {
     )
   )
 }
-
+/*
 # Created as a seperate resource, as managed identity uses the azurerm_key_vault_access_policy as well. See note at https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault_access_policy
 resource "azurerm_key_vault_access_policy" "deployment_spn_access_policy" {
   for_each     = local.deployment_client_ids
@@ -58,4 +58,4 @@ resource "azurerm_key_vault_access_policy" "deployment_spn_access_policy" {
     "Update",
     "Create"
   ]
-}
+}*/
