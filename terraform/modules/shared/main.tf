a499ec9ecaa6923dbc3e6382b90ea20480bf977d
Joachim Huber <joachim.huber@ipt.ch>
2023-07-17 11:49:34 +0200

    Enable user

--- terraform/modules/shared/main.tf
+++ terraform/modules/shared/main.tf
@@ -35,28 +35,28 @@ locals {
 
 
 # Created as a seperate resource, as managed identity uses the azurerm_key_vault_access_policy as well. See note at https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault_access_policy
-#resource "azurerm_key_vault_access_policy" "deployment_spn_access_policy" {
-#  for_each     = local.deployment_client_ids
-#  key_vault_id = azurerm_key_vault.key_vault.id
-#  tenant_id    = data.azurerm_client_config.current.tenant_id
-#  object_id    = each.value
-#
-#  key_permissions = [
-#    "Get",
-#  ]
-#
-#  secret_permissions = [
-#    "Get",
-#  ]
-#
-#  storage_permissions = [
-#    "Get",
-#  ]
-#  certificate_permissions = [
-#    "Import",
-#    "Get",
-#    "List",
-#    "Update",
-#    "Create"
-#  ]
-#}
+resource "azurerm_key_vault_access_policy" "deployment_spn_access_policy" {
+  for_each     = local.deployment_client_ids
+  key_vault_id = azurerm_key_vault.key_vault.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = each.value
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
+  certificate_permissions = [
+    "Import",
+    "Get",
+    "List",
+    "Update",
+    "Create"
+  ]
+}
